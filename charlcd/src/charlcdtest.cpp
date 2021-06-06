0e7fdbe3c967197960910bab5fcfe743ab3a7c83
nvitya <nvitya@gmail.com>
2021-06-06 16:28:09 +0200

    bitbang 4-bit character lcd

--- charlcd/src/charlcdtest.cpp
+++ charlcd/src/charlcdtest.cpp
@@ -1,14 +1,18 @@
 // chartest.cpp
 
-#include "charlcd_i2c.h"
 #include "traces.h"
 #include "clockcnt.h"
+#include "hwpwm.h"
 
-#define DISPLAY_BITBANG 0
+#define DISPLAY_BITBANG 1
 
 #if DISPLAY_BITBANG
+  #include "charlcd_bb4.h"
+
   TCharLcd_bb4  disp;
 #else
+  #include "charlcd_i2c.h"
+
   TCharLcd_i2c  disp;
 #endif
 
@@ -19,72 +23,64 @@
 uint8_t cdisp_chars[CDISP_CHAR_NUM];
 uint8_t cdisp_changemap[CDISP_CHAR_NUM / 8];
 
+THwPwmChannel  conpwm;
+
+TCharLcdCtrlType  ctrltype = CHLCD_CTRL_HD44780;
+
 void charlcd_test()
 {
 	TRACE("Character LCD display test\r\n");
 
 #if 0
 
-#elif defined(BOARD_MIN_F103)
-
-	// LCD control
-	hwpinctrl.PinSetup(PORTNUM_A, 5, PINCFG_OUTPUT | PINCFG_AF_0); // SPI1_SCK
-	hwpinctrl.PinSetup(PORTNUM_A, 7, PINCFG_OUTPUT | PINCFG_AF_0); // SPI1_MOSI
-
-	disp.pin_reset.Assign(PORTNUM_B, 0, false);
-	disp.pin_reset.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1); // B0: RESET
-
-	disp.pin_cs.Assign(PORTNUM_A, 4, false);
-	disp.pin_cs.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-	disp.pin_cd.Assign(PORTNUM_B, 1, false);
-	disp.pin_cd.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-	// SPI1
-
-	//disp.txdma.Init(1, 3, 1);  // SPI1/TX = DMA channel 3
-
-	disp.spi.idleclk_high = false;
-	disp.spi.datasample_late = false;
-	disp.spi.speed = 4000000; // 4 MHz
-	disp.spi.Init(1);
-
 #elif defined(BOARD_MIBO48_STM32F303)
 
-	// opendrain allows 5V I2C signal driving, because the pins are 5V tolerant
+	#if DISPLAY_BITBANG
 
-	// LCD control
-	hwpinctrl.PinSetup(PORTNUM_B, 6, PINCFG_AF_4 | PINCFG_OPENDRAIN); // I2C1_SCL
-	hwpinctrl.PinSetup(PORTNUM_B, 7, PINCFG_AF_4 | PINCFG_OPENDRAIN); // I2C1_SDA
+		// the 2x16 character display connected over TI 3.3V -> 5V level shifter
 
-	// I2C1
+		ctrltype = CHLCD_CTRL_ST7066U;
 
-	disp.i2c.speed = 100000; // 100 kHz
-	disp.i2c.Init(1);
+		disp.pin_en.Assign(PORTNUM_B, 0, false);
+		disp.pin_rs.Assign(PORTNUM_B, 1, false);
+		disp.pin_rw.Assign(PORTNUM_B, 2, false);
 
-	disp.cols = 16;
-	disp.rows = 2;
+		disp.pin_data[0].Assign(PORTNUM_A, 4, false);
+		disp.pin_data[1].Assign(PORTNUM_A, 5, false);
+		disp.pin_data[2].Assign(PORTNUM_A, 6, false);
+		disp.pin_data[3].Assign(PORTNUM_A, 7, false);
 
+		hwpinctrl.PinSetup(PORTNUM_B, 3, PINCFG_AF_1); // select TIM2_CH2
 
-#elif defined(BOARD_MIBO64_ATSAM4S)
+		conpwm.frequency = 10000;
+		conpwm.Init(2, 2, 0); // Timer2/Ch2/output0
+		conpwm.Enable();
+		conpwm.SetOnClocks((conpwm.periodclocks * 30) / 100);
 
-	disp.pin_clk.Assign(PORTNUM_B, 0, false);
-	disp.pin_clk.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_0);
+		disp.cols = 16;
+		disp.rows = 2;
 
-	disp.pin_din.Assign(PORTNUM_B, 1, false);
-	disp.pin_din.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_0);
+	#else
+		// opendrain allows 5V I2C signal driving, because the pins are 5V tolerant
 
-	disp.pin_ce.Assign(PORTNUM_B, 2, false);
-	disp.pin_ce.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_0);
+		// LCD control
+		hwpinctrl.PinSetup(PORTNUM_B, 6, PINCFG_AF_4 | PINCFG_OPENDRAIN); // I2C1_SCL
+		hwpinctrl.PinSetup(PORTNUM_B, 7, PINCFG_AF_4 | PINCFG_OPENDRAIN); // I2C1_SDA
 
-	disp.pin_reset.Assign(PORTNUM_B, 3, false);
-	disp.pin_reset.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1); // B0: RESET
+		// I2C1
+
+		disp.i2c.speed = 100000; // 100 kHz
+		disp.i2c.Init(1);
+
+		disp.cols = 16;
+		disp.rows = 2;
+	#endif
 
 #else
   #error "unknown board."
 #endif
 
-	if (!disp.Init(CHLCD_CTRL_HD44780, disp.cols, disp.rows, &cdisp_chars[0], &cdisp_changemap[0]))
+	if (!disp.Init(ctrltype, disp.cols, disp.rows, &cdisp_chars[0], &cdisp_changemap[0]))
 	{
 		TRACE("Error initializing display!\r\n");
 		return;
